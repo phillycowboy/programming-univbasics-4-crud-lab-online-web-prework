04ecb898506862c768d39b8c5b8302f8d2648a16
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-23 16:56:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   groceries=["beer","cheese","eggs","milk"]
-  groceries.unshift("arrays!")
+  groceries.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
