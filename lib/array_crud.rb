d21e42ef7c2c2f65a537f819d09ad584cda66442
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-23 17:09:40 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,47 @@
 def create_an_empty_array
-  
+  my_array=[]
 end
 
 def create_an_array
-  
+  groceries=["beer","cheese","eggs","milk"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    groceries=["beer","cheese","eggs","milk"]
+    groceries.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  groceries=["beer","cheese","eggs","milk", "arrays!"]
+  groceries.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  groceries=["wow","beer","cheese","eggs","milk","arrays!"]
+  groceries.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+    groceries=["wow","beer","cheese","eggs","milk"]
+    groceries.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+times_of_day=["dusk", "pm","am","dawn"]
+times_of_day[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  sayings=["wow", "omg", "lol"]
+  sayings[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  my_array=["list", "data", "arrays!"]
+  my_array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+this_is_my_list=["wow", "arrays", "donkey feed", "foo","totally"]
+this_is_my_list[4]
 end
