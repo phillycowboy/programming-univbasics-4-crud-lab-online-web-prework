85408325cfe740b3c6519a3b625a898bc355db43
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-23 16:54:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
     groceries=["beer","cheese","eggs","milk"]
-    groceries.push("muffins")
+    groceries.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
