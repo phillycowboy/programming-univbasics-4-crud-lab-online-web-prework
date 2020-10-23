1229e0abd9ba53d4f2bddd2ce18e747e5a556ec8
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-23 16:58:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  groceries=["beer","cheese","eggs","milk"]
+  groceries=["wow","beer","cheese","eggs","milk","arrays!"]
   groceries.pop()
 end
 
